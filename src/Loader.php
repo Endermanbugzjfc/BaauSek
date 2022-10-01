778bd1b33fde379a35a38a5124ef8c8b43e72ca9
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-10-01 19:42:05 +0800

    Poop logic.

---
+++ src/Loader.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\ExcretionMechanism;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase implements Listener {
+
+	protected function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onPlayerJoinEvent(PlayerJoinEvent $event) : void {
+		new Session($event->getPlayer());
+	}
+
+
+}
--- src/Session.php
+++ src/Session.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\ExcretionMechanism;
 
+use SOFe\AwaitGenerator\f2c;
 use SOFe\AwaitStd\AwaitStd;
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\VanillaEffects;
@@ -23,7 +24,7 @@ class Session {
 
 	private function loopPlayerExhaustEvent() : \Generator {
 		while (true) {
-			yield from $this->std(
+			yield from $this->std->awaitEvent(
 				PlayerExhaustEvent::class,
 				fn(PlayerExhaustEvent $event) => $event->getPlayer() === $this->store->getPlayer(),
 				false,
@@ -46,4 +47,41 @@ class Session {
 			}
 		}
 	}
-}
+
+	private function loopPlayerToggleSneakEvent() : void {
+		while (true) {
+			yield from $this->nextPlayerToggleSneakEvent(true);
+			// 「醞釀屎意」
+			$hatchUp = new PlayerPoopPreExcretEvent();
+			$hatchUp->call();
+
+			// Poop whenever $hatchUp->await() cycles until player unseaks.
+			while ($this->store->getPlayer()->isSneaking()) {
+				[, $event] = yield from Await::race([
+					$hatchUp->await(),
+					$this->nextPlayerToggleSneakEvent(false)
+				]);
+				if ($event instanceof PlayerToggleSneakEvent) {
+					break;
+				}
+
+				$excret = new PlayerPoopExcretEvent();
+				$excret->call();
+				if (!$excret->isCancelled()) {
+					$pos = $excert->getPosition();
+					$pos->getWorld()->dropItem($pos, $excret->getItem(), $excret->getMotion());
+				}
+			}
+		}
+	}
+
+	private function nextPlayerToggleSneakEvent(bool $sneak) : \Generator {
+		yield from $this->std->awaitEvent(
+			PlayerToggleSneakEvent::class,
+			fn(PlayerToggleSneakEvent $event) => $event->getPlayer() === $this->store->getPlayer() && $event->isSneaking() === $sneak,
+			false,
+			EventPriority::MONITOR,
+			false,
+			$this->store->getPlayer()
+		);
+	}
